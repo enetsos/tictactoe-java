a3dd13ce4f9142fbb2ddbdafa2c12f52e4ed0cf7
MatteoArna <matteo.arena711@gmail.com>
2023-03-13 11:53:49 +0100

    Project Scaffolding

---
+++ tictactoe/frontend/src/main/java/ch/supsi/tictactoe/App.java
@@ -0,0 +1,13 @@
+package ch.supsi.tictactoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
